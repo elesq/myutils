727eea2d19df634704d0ce05295666d76483ed64
elesq <87203224+elesq@users.noreply.github.com>
2021-11-23 20:55:15 +0000

    added types for jest

--- scripts/create-react-jest-babel-app.sh
+++ scripts/create-react-jest-babel-app.sh
@@ -23,8 +23,11 @@ echo "react project with jest & babel"
 git clone git@github.com:elesq/react-jest-babel-kit.git $1
 echo -e "\xE2\x9C\x94 react, jest & babel project cloned as" $1
 
-# change into project directory
-cd $1
+
+# change into project directory, remove existing git origin
+echo "Cleaning repository status and removing remote..."
+cd $1 && git remote rm origin
+
 
 # babel
 npm i --save-dev @babel/core
@@ -38,6 +41,9 @@ npm i --save-dev jest
 
 # react
 npm i --save react react-dom
+
+# jest types
+npm install @types/jest
 
 echo -e "\xE2\x9C\x94 added babel dev packages"
 echo -e "\xE2\x9C\x94 added babel runtime"
